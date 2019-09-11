5864eff832b1a63d90ff919b799006d5cc56394c
Willy Tekeu <willy.tekeu@gmail.com>
2019-09-11 18:06:35 -0400

    Simplifies code and fixes eslint

--- src/fn/upload-app-settings.js
+++ src/fn/upload-app-settings.js
@@ -1,22 +1,11 @@
 const fs = require('../lib/sync-fs');
 const request = require('request-promise-native');
 const skipFn = require('../lib/skip-fn');
-const pouch = require('../lib/db');
-// const warnUploadOverwrite = require('../lib/warn-upload-overwrite');
 
 module.exports = async (projectDir, couchUrl) => {
   const body = fs.read(`${projectDir}/app_settings.json`);
 
   if(!couchUrl) return skipFn('no couch URL set');
-
-  const db = pouch(couchUrl);
-
-  const doc = {
-    _id: 'settings',
-    settings: JSON.parse(body)
-  };
-
-  // await warnUploadOverwrite.preUpload(projectDir, db, doc, couchUrl);
 
   const serverUrl = couchUrl.slice(0, couchUrl.lastIndexOf('/'));
   await request.put({
@@ -25,8 +14,6 @@ module.exports = async (projectDir, couchUrl) => {
       headers: { 'Content-Type':'application/json' },
       body: body,
   });
-
-  // await warnUploadOverwrite.postUpload(projectDir, db, doc, couchUrl);
 
   return Promise.resolve();
 };
